01bce616f762dc0da85dcb1fab414d1b833d9349
Sarfaraz <ksarfarazahamed@manh.com>
2018-06-02 00:05:45 +0530

    My first commit

---
+++ AnotherSample.java
@@ -0,0 +1,16 @@
+
+public class AnotherSample
+{
+
+    public AnotherSample()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ Sample.java
@@ -0,0 +1,16 @@
+
+public class Sample
+{
+
+    public Sample()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
